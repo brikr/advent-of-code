7054015c52e5289bb27af71a03d5b0769122754c
Brik <brikr@users.noreply.github.com>
2023-01-31 16:39:47 -1000

    2019 day 6

--- src/2019/day06/day06.ts
+++ src/2019/day06/day06.ts
@@ -1,16 +1,68 @@
 import {fileMapSync} from '../../utils/file';
+import {MapWithDefault} from '../../utils/mapWithDefault';
 import {printSolution} from '../../utils/printSolution';
 
-function part1(lines: string[]): number {
+function part1(orbits: string[][]): number {
+  // key: planet, value: planet it orbits
+  const orbitMap = new Map<string, string>();
+  for (const [parent, planet] of orbits) {
+    orbitMap.set(planet, parent);
+  }
+  // console.log(orbitMap);
+
+  let total = 0;
+  for (const planet of orbitMap.keys()) {
+    let orbits = 0;
+    let curr = planet;
+    while (curr !== 'COM') {
+      curr = orbitMap.get(curr)!;
+      orbits++;
+    }
+    // console.log(planet, 'has', orbits, 'orbits');
+    total += orbits;
+  }
+  return total;
+}
+
+function part2(orbits: string[][]): number {
+  // key: planet, value: planet it orbits
+  const orbitMap = new Map<string, string>();
+  for (const [parent, planet] of orbits) {
+    orbitMap.set(planet, parent);
+  }
+
+  // key: planet, value: list of parents in order up to COM
+  const parentListMap = {
+    YOU: new Array<string>(),
+    SAN: new Array<string>(),
+  };
+  for (const planet of ['YOU', 'SAN'] as const) {
+    const parentList: string[] = [];
+    let curr: string | undefined = planet;
+    while (curr) {
+      curr = orbitMap.get(curr);
+      if (curr) {
+        parentList.push(curr);
+      }
+    }
+    parentListMap[planet] = parentList;
+  }
+  // console.log(parentListMap);
+
+  for (const [youDistance, planet] of parentListMap.YOU.entries()) {
+    const sharedParentSanDistance = parentListMap.SAN.findIndex(
+      p => p === planet
+    );
+
+    if (sharedParentSanDistance !== -1) {
+      // found!
+      return youDistance + sharedParentSanDistance;
+    }
+  }
+
+  // woops
   return 0;
 }
 
-function part2(lines: string[]): number {
-  return 0;
-}
-
-const lines = fileMapSync(
-  'src/2019/day06/input-test.txt',
-  line => line
-);
-printSolution(part1(lines), part2(lines));
+const orbits = fileMapSync('src/2019/day06/input.txt', line => line.split(')'));
+printSolution(part1(orbits), part2(orbits));
